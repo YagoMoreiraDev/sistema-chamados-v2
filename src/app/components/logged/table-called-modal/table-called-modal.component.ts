97d3de022f3f60a3331425b900f9017157ad9795
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-09-24 07:19:18 -0300

    Atualização do table called modal

---
+++ src/app/components/logged/table-called-modal/table-called-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-called-modal',
+  imports: [],
+  templateUrl: './table-called-modal.component.html',
+  styleUrl: './table-called-modal.component.scss'
+})
+export class TableCalledModalComponent {
+
+}
